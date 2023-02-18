37bad9a8aedaa0bd0b5e33158f70d3c262145abd
gillus <gillus@gmail.com>
2023-02-18 14:12:18 +0100

    Version 0 exercise

---
+++ data/data_preparation.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def prepare_data(data_path: str, parameters=None):
+    # This function trains a random folder classifier using the data specified by datapath
+    # If parameters are not specified as argument look for params.json file, otherwise create default values
+    if parameters is None:
+        if os.path.exists('./params_data_prep.json'):
+            parameters = json.load(open("parparams_data_prep.json", "r"))
+        else:
+            parameters = dict(test_size=0.2, val_size=0.2, undersampling=0.75)
+
+    raw_data = pd.read_csv(data_path)
+    data = raw_data.iloc[:int(raw_data.shape[0]*(1-parameters['test_size'])), :]
+    holdout_data = raw_data.iloc[int(raw_data.shape[0]*(1-parameters['test_size'])):, :]
+    train_data, val_data = train_test_split(data, test_size=parameters['val_size'])    
+    x1 = train_data[train_data['income']=='>50k']
+    x2 = train_data[train_data['income']!='>50k']
+    train_data = pd.concat([x1, x2.sample(frac=parameters['undersampling'])], axis=0).sample(frac=1.)
+
+    train_data.to_csv('train.csv', index=False)
+    val_data.to_csv('val.csv', index=False)
+    holdout_data.to_csv('holdout.csv', index=False)
+
+
+    return 
+
+
+if __name__ == '__main__':
+
+    prepare_data('./data/raw_data.csv')
--- model/model_training.py
+++ model/model_training.py
@@ -3,14 +3,25 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from lightgbm import LGBMClassifier
 import json
 import os
 import joblib
-from data.datamanager import data_loader
+import pandas as pd
 
 
-def train_random_forest_model(data_path: str,
-                              parameters=None):
+def data_loader(path: str):
+    data_csv_path = path
+    dataset = pd.read_csv(data_csv_path)
+
+    target_column = 'income'
+    y = dataset[target_column]
+    x = dataset.drop(target_column, axis=1)
+    return x, y
+
+
+def train_random_forest_model(parameters=None):
     # This function trains a random folder classifier using the data specified by datapath
     # If parameters are not specified as argument look for params.json file, otherwise create default values
     if parameters is None:
@@ -20,37 +31,38 @@ def train_random_forest_model(data_path: str,
             parameters = dict(n_estimators=100, max_depth=4, criterion='gini',
                               min_sample_leaf=10)
     print(parameters)
-    x_training, y_training = data_loader(data_path)
-
+    x_training, y_training = data_loader('./train.csv')
+    x_val, y_val = data_loader('./val.csv')
 
     # Scikit learn ColumnTransformer used to process ordinal and nominal data
     ordinal_features = x_training.select_dtypes(include="number").columns
     categorical_features = x_training.select_dtypes(include="object").columns
 
-    ordinal_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
+    numerical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='median')),
                                           ('scaler', StandardScaler())])
 
-    categorical_transformer = Pipeline(steps=[('onehot', OneHotEncoder(handle_unknown='ignore'))])
+    categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(strategy='most_frequent')),
+                                              ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-    x_encoder = ColumnTransformer(transformers=[('ord', ordinal_transformer, ordinal_features),
+    x_encoder = ColumnTransformer(transformers=[('num', numerical_transformer, ordinal_features),
                                                 ('cat', categorical_transformer, categorical_features)])
 
-
-    rf_clf = RandomForestClassifier(n_estimators=parameters['n_estimators'],
-                                    max_depth=parameters['max_depth'],
-                                    criterion=parameters['criterion'],
-                                    min_samples_leaf=parameters['min_sample_leaf'],
-                                    random_state=42)
+    print(parameters)
+    rf_clf = LGBMClassifier(n_estimators=parameters['n_estimators'],
+                            max_depth=parameters['max_depth'],
+                            random_state=42)
 
     rf_pipeline = Pipeline(steps=[("preprocessing", x_encoder), ("rf_model", rf_clf)])
     rf_pipeline.fit(x_training, y_training)
 
+    pred_val = rf_pipeline.predict(x_val)
+    metrics = classification_report(y_val,pred_val, output_dict=True)
     # serialize model using joblib
     joblib.dump(rf_pipeline, 'model.pkl')
-
+    json.dump(metrics, open("metrics.json", "w"))
     return rf_pipeline
 
 
 if __name__ == '__main__':
 
-    train_random_forest_model('./data/adult_training.csv')
+    train_random_forest_model()
